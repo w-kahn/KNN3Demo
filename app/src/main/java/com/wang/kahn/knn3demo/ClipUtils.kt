cffae037707af1d0f590e7669247db684a129d8c
kahn <w_kahn@163.com>
2022-05-31 10:15:48 +0800

    add share and download feature

--- app/src/main/java/com/wang/kahn/knn3demo/ClipUtils.kt
+++ app/src/main/java/com/wang/kahn/knn3demo/ClipUtils.kt
@@ -1,11 +1,82 @@
 package com.wang.kahn.knn3demo
 
-import android.content.ClipData
-import android.content.ClipboardManager
-import android.content.Context
+import android.app.Activity
+import android.app.DownloadManager
+import android.content.*
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Environment
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.annotation.Nullable
+import androidx.core.content.FileProvider
+import androidx.fragment.app.FragmentActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import java.io.File
+import java.io.FileInputStream
+import java.io.IOException
+import java.io.InputStream
 
+
+const val DIR_NAME = "Web3Telescope"
 fun copyToClipboard(context : Context, content: String) {
     val clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
     val clip = ClipData.newPlainText("address", content)
     clipboard.setPrimaryClip(clip)
+}
+
+fun downloadImage(imageUrl: String, context: Context) {
+    val direct = File(
+        Environment
+            .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+            .absolutePath + "/" + DIR_NAME + "/"
+    )
+    if (!direct.exists()) {
+        direct.mkdir()
+    }
+
+    val fileName = createName(imageUrl)
+
+    var mimetype = "image/png"
+    if (fileName.endsWith(".gif")) {
+        mimetype = "image/gif"
+    }
+
+    val downloadUri: Uri = Uri.parse(imageUrl)
+    val request = DownloadManager.Request(downloadUri)
+    request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI or DownloadManager.Request.NETWORK_MOBILE)
+        .setAllowedOverRoaming(false)
+        .setTitle(fileName)
+        .setMimeType(mimetype)
+        .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+        .setDestinationInExternalPublicDir(
+            Environment.DIRECTORY_PICTURES,
+            File.separator + DIR_NAME + File.separator + fileName
+        )
+
+    (context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager).enqueue(request)
+}
+
+
+fun createName(url: String): String {
+    var name = url.substring(url.lastIndexOf('/') + 1, url.length)
+    val NoExt = name.substring(0, name.lastIndexOf('.'))
+    val ext = name.substring(name.lastIndexOf('.'))
+    if (!ext.equals(".gif")) {
+        name = "$NoExt.png"
+    }
+    return name
+}
+
+fun shareImage(file: File, activity: Activity) {
+    val fileUri = FileProvider.getUriForFile(activity, "com.wang.kahn.knn3demo.fileprovider", file)
+    val shareIntent = Intent().apply {
+        action = Intent.ACTION_SEND
+        putExtra(Intent.EXTRA_STREAM, fileUri)
+        type = "image/jpeg"
+    }
+    activity.startActivity(Intent.createChooser(shareIntent, "Share To"))
 }
--- app/src/main/java/com/wang/kahn/knn3demo/DetailFragment.kt
+++ app/src/main/java/com/wang/kahn/knn3demo/DetailFragment.kt
@@ -1,14 +1,18 @@
 package com.wang.kahn.knn3demo
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import android.widget.Toast
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.wang.kahn.knn3demo.databinding.FragmentDetailBinding
+import java.io.File
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -36,6 +40,23 @@ class DetailFragment : Fragment() {
         Glide.with(view.context).load(args.nftUrl).into(binding.nftDetailImage)
         binding.nftSymbolText.text = args.nftSymbol
         binding.nftContractAddress.text = args.nftContract
+        binding.downloadButton.setOnClickListener {
+            downloadImage(args.nftUrl!!, view.context)
+            Toast.makeText(view.context,R.string.start_download,Toast.LENGTH_SHORT).show()
+        }
+
+        binding.shareButton.setOnClickListener{
+            Glide.with(view.context).download(args.nftUrl).into(object : CustomTarget<File>() {
+                override fun onResourceReady(resource: File, transition: Transition<in File>?) {
+                    activity?.let { it1 -> shareImage(resource, it1) }
+                }
+
+                override fun onLoadCleared(placeholder: Drawable?) {
+
+                }
+
+            })
+        }
     }
 
     override fun onDestroyView() {
